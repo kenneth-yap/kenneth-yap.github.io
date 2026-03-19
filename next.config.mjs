68e49e86e7781dc66a799147ff6befc202b3b550
Kenneth Yap <85454120+kenneth-yap@users.noreply.github.com>
2026-03-19 12:27:44 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-   /** @type {import('next').NextConfig} */
-   const nextConfig = {
-    images: {
-      unoptimized: true,
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
 
-  export default nextConfig;
+export default nextConfig;
